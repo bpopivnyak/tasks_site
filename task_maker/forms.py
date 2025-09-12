fb1a71bc190364e5300fa82fc3bd0569f908ea48
Anonymous <141341450+bpopivnyak@users.noreply.github.com>
2025-09-12 19:38:19 +0300

    done

--- task_maker/forms.py
+++ task_maker/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Task
+from .models import Task, Project
+
+
 
 class TaskForm(forms.ModelForm):
-     class Meta:
-         model = Task
-         fields = ["title", "description", "user", "status"]
+
+    class Meta:
+        model = Task
+        fields = ["title", "description", "user", "status"]
+
+
+
--- task_maker/models.py
+++ task_maker/models.py
@@ -5,6 +5,9 @@ class Project(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
 
+    def __str__(self):
+        return self.title
+
 
 class Task(models.Model):
     title = models.CharField(max_length=255)
